623c916f70ffa4849bd95ddbc7140d00b532116c
Bruno Bigras <bigras.bruno@gmail.com>
2022-12-06 17:59:44 -0500

    try @neondatabase/serverless

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,12 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Link } from "@builder.io/qwik-city";
 
+import { Client } from "@neondatabase/serverless";
+
 export default component$(() => {
+  const client = new Client("fake url");
+  console.log("client", client);
+
   return (
     <div>
       <h1>
